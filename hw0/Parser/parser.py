f79f5557fed6de4b2da56732a95cdb88f38774a8
Nero Zuo <502350123@qq.com>
2017-03-16 00:14:38 +0800

    Finish

--- hw0/Parser/parser.py
+++ hw0/Parser/parser.py
@@ -26,31 +26,10 @@ class Parser(object):
 
     def parseFiles(self):
         contents = ""
-        files = flatMap(filesInPath, self.paths)
-        for file in files:
+        for file in self.paths:
             with open(file, 'r') as f:
                 contents += " "
                 contents += f.read()
         return self.parse(contents)
 
 
-from os import listdir
-from os.path import isfile, join, exists
-# get all the files in the given path
-def filesInPath(path):
-    if not exists(path):
-        return []
-    if isfile(path):
-        return [path]
-    files = []
-    for f in listdir(path):
-        p = join(path, f)
-        if isfile(p):
-            files.append(p)
-        else:
-            files.extend(filesInPath(p))
-    return files
-
-def flatMap(func, seq):
-    listOfLists = map(func, seq)
-    return reduce(list.__add__, listOfLists)
--- hw0/main.py
+++ hw0/main.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 from Parser import Parser
 import optparse
+import os
 
-def printDic(dic):
-    for key in dic:
-        print('{:15}{:3}'.format(key,dic[key]))
+def printRes(res, isDesc = None):
+    if isDesc is not None:
+        res = OrderedDict(sorted(res.items(), key=lambda t: t[1], reverse=isDesc))
+    for key in res:
+        print('{:15}{:3}'.format(key, res[key]))
+
 
 def parse_args():
     usage = """ usage: %prog [options] path ...
@@ -20,12 +24,66 @@ And it will print the words frequency in the given files or the files in the giv
     p = optparse.OptionParser(usage)
 
     help = "Print the results by the order of frequency. Default is in desc order"
-    p.add_option('--order', help=help, default="desc")
+    p.add_option('--order', help=help)
 
-    help
+    help = "Recursive count the files in the given path"
+    p.add_option("-r", action="store_true", dest="recursive", help=help, default=False)
+
+    options, args = p.parse_args()
+
+    if options.order is not None:
+        if options.order == "asc":
+            options.order = False
+        elif options.order == "desc":
+            options.order = True
+        else:
+            p.error("--order must equal to 'asc' or 'desc")
+
+    if len(args) != 1:
+        p.error("Provide at least one file or directory")
+
+    for path in args:
+        if not os.path.exists(path):
+            p.error("No such file: %s" % path)
+
+    return options, args
+
+from os import listdir
+from os.path import isfile, join, exists
+# get all the files in the given path
+# It is a curry function
+def filesInPath(isRec):
+    def innerFunc(path):
+        if not exists(path):
+            return []
+        if isfile(path):
+            return [path]
+        files = []
+        for f in listdir(path):
+            p = join(path, f)
+            if isfile(p):
+                files.append(p)
+            elif isRec:
+                files.extend(filesInPath(isRec)(p))
+        return files
+    return innerFunc
+
+
+def flatMap(func, seq):
+    listOfLists = map(func, seq)
+    return reduce(list.__add__, listOfLists)
+
+
+from collections import OrderedDict
+def main():
+    options, paths = parse_args()
+    # files = filesInPath(paths[0], options.recursive)
+    files = flatMap(filesInPath(options.recursive), paths)
+    parser = Parser(*files)
+    res = parser.parseFiles()
+    printRes(res, isDesc=options.order)
 
 
 
 if __name__ == '__main__':
-    p = Parser('sources')
-    printDic(p.parseFiles())
+    main()
