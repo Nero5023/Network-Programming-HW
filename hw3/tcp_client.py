3d4a4ab90f668dbe2ccab1cb7fc74ee1bd6319bb
Nero Zuo <502350123@qq.com>
2017-05-19 00:20:53 +0800

    add select to tcp_client

--- hw3/tcp_client.py
+++ hw3/tcp_client.py
@@ -1,16 +1,31 @@
 #coding:utf-8
 
 import socket
+import select
+import sys
+
 HOST = 'localhost'
 PORT = 8080
 
+inputFS = [sys.stdin]
+s = None
 while True:
-    temp = raw_input("input> ")
-    if temp == 'exit':
-        break
-    s = socket.socket()
-    s.connect((HOST, PORT))
-    s.send(temp)
-    data = s.recv(1024)
-    print "Got from server: '%s'" % (data,)
-    s.close()
+    infs, outfs, excs = select.select(inputFS, [], [])
+    for x in infs:
+        if x is sys.stdin:
+            data = sys.stdin.readline()
+            if data[-1] == '\n':
+                data = data[:len(data)-1]
+            s = socket.socket()
+            s.connect((HOST, PORT))
+            s.send(data)
+            inputFS.append(s)
+        if x is s:
+            s.recv(1024)
+            print "Got from server: '%s'" % (data,)
+            s.close()
+            inputFS.remove(s)
+            s = None
+
+
+
