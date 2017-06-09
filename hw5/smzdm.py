370b8ed272c4c220e8601c402f1641d191c95d26
Nero Zuo <502350123@qq.com>
2017-06-09 22:46:39 +0800

    fix recursive

--- hw5/smzdm.py
+++ hw5/smzdm.py
@@ -2,23 +2,8 @@
 
 import urllib2
 from lxml import etree
-# f = urllib2.urlopen("http://www.smzdm.com/tag/%E4%BA%AC%E4%B8%9C618/faxian/")
-# firstLine = f.read()
-# print firstLine
 
-# url = "http://www.smzdm.com/tag/%E4%BA%AC%E4%B8%9C618/faxian/"
-# req = urllib2.Request(url)
-# req.add_header('User-Agent', 'Mozilla/5.0 \
-#     (Macintosh; Intel Mac OS X 10_11_6) \
-#     AppleWebKit/537.36 (KHTML, like Gecko) \
-#     Chrome/58.0.3029.110 Safari/537.36')
-# reponse = urllib2.urlopen(req)
-# page = reponse.read()
-# root = etree.HTML(page)
-# itemsListXPATH = "//li[@class='list']"
-# allItems = root.xpath(itemsListXPATH)
-
-# return the next page url
+# 递归算法
 def getItemsInPage(url):
     req = urllib2.Request(url)
     req.add_header('User-Agent', 'Mozilla/5.0 \
@@ -61,7 +46,7 @@ def getItemsInPage(url):
     pageDownLinkXPATH = "//li[@class='pagedown']/a"
     pageDownNodes = root.xpath(pageDownLinkXPATH)
     if len(pageDownNodes) == 0:
-        print "--"
+        return 
     else:
         pageDownLink = pageDownNodes[0].attrib['href']
         getItemsInPage(pageDownLink)
